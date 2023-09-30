08ee15ca4855803f81149a1a9dea13116fd452fa
AMANQAZPLM <amanqazplm@gmail.com>
2023-09-30 15:33:12 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,23 +55,25 @@ const Home = () => {
   return (
     <div className="home">
       <div className="posts">
-        {posts.map((post) => (
-          <div className="post" key={post.id}>
-            <div className="img">
-              <img src={`../upload/${post.img}`} alt="" />
-            </div>
-            <div className="content">
-              <Link className="link" to={`/post/${post.id}`}>
-                <h1>{post.title}</h1>
-              </Link>
-              <p>{getText(post.desc)}</p>
+        {posts &&
+          posts.map &&
+          posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="img">
+                <img src={`../upload/${post.img}`} alt="" />
+              </div>
+              <div className="content">
+                <Link className="link" to={`/post/${post.id}`}>
+                  <h1>{post.title}</h1>
+                </Link>
+                <p>{getText(post.desc)}</p>
 
-              <Link className="link" to={`/post/${post.id}`}>
-                <button>Read More</button>
-              </Link>
+                <Link className="link" to={`/post/${post.id}`}>
+                  <button>Read More</button>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
